d3786dd3ea24621ba3ade011d519de9604c019c4
shivpateriya <shivampateriya40@gmail.com>
2023-07-10 15:32:39 +0530

    change in the file

--- addloggin.py
+++ addloggin.py
@@ -24,3 +24,14 @@ logging.warning("Something unexpected happened!")
 
 print("hello world")
 logging.info("Script completed successfully.")
+
+
+# Get the current date
+current_date = datetime.date.today().strftime("%Y-%m-%d")
+
+# If the log file is from a previous day, clear it
+if log_filename != f"{current_date}.log":
+    with open(log_filename, "w"):
+        pass
+    log_filename = f"{current_date}.log"
+    logging.basicConfig(filename=log_filename, level=logging.INFO, format=log_format)
