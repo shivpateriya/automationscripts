2980e937455aba7d9b6007b592074f6d60e1d721
shivpateriya <shivampateriya40@gmail.com>
2023-09-07 16:13:28 +0530

    push

--- ingestionlog3.py
+++ ingestionlog3.py
@@ -2,7 +2,6 @@ import glob
 import os
 import requests
 import csv
-import re
 from datetime import datetime
 
 # Define the directory containing the log files
@@ -20,27 +19,24 @@ log_files = glob.glob(pattern)
 # Create a set to store unique tabular data as tuples
 unique_tabular_data = set()
 
-# Define regular expressions for "Unsuccessfully published" and "ConfigurationExport"
-unsuccessful_pattern = re.compile(r"Unsuccessfully published")
-config_export_pattern = re.compile(r"ConfigurationExport.*")
-
 # Iterate over each log file
 for file in log_files:
     capture_data = False
     tabular_data = []  # Store tabular data lines
+    file_name = None
 
-    # Open the log file and search for lines containing "Unsuccessfully published"
+    # Open the log file and search for lines containing both "ERROR" and "Unsuccessfully published" in the same line
     with open(file, encoding="utf-8") as f:
         for line in f:
-            if unsuccessful_pattern.search(line):
+            if "ERROR" in line and "Unsuccessfully published" in line:
                 capture_data = True
-            elif capture_data:
-                if config_export_pattern.search(line):
-                    file_name = line.strip()  # Get the filename
-                    capture_data = False  # Stop capturing data after filename
-                elif line.strip() and not line.startswith("serialNo extSensorID meterStatusIEE"):
+                # Extract the filename from the line
+                file_name = line.split('{')[1].split('}')[0].strip()
+                continue
+            if capture_data and line.strip():
+                if not line.startswith("sensorID smpID startTS endTS seriallo MDL_REF_smpID"):
                     data = line.strip().split()  # Split the line into columns
-                    if len(data) == 9:  # Assuming 9 columns in the tabular data
+                    if len(data) == 6:  # Assuming 6 columns in the tabular data
                         data_tuple = tuple(data)  # Convert the data list to a tuple
                         unique_tabular_data.add((file_name,) + data_tuple)  # Add the filename to the tuple and add it to the set
 
@@ -51,7 +47,7 @@ csv_file_name = f"PbcExportUnpublishedTabularData{current_date}dyamanica.csv"
 # Create CSV file from the unique tabular data with the specified header
 with open(csv_file_name, 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['filename', 'serialNo', 'extSensorID', 'meterStatusIEE', 'startTs', 'endTs', 'sensorID', 'status', 'deviceOperationalStatus', 'meterProgramID'])
+    csv_writer.writerow(['filename', 'sensorID', 'smpID', 'startTS', 'endTS', 'serialNo', 'MDL_REF_smpID'])
     csv_writer.writerows(unique_tabular_data)
 
 # Send alert to Teams
