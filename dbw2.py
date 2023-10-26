c4bea677d9441d5ea098dc737ebfe3300ece423a
shivpateriya <shivampateriya40@gmail.com>
2023-10-26 14:05:39 +0530

    new file

--- dbw2.py
+++ dbw2.py
@@ -1,6 +1,6 @@
 import os
 
-# Define the name of the input file and the not_found file
+# Define the name of the input file
 input_file = "dbw.dat"
 not_found_file = "not_found.txt"
 
@@ -15,23 +15,21 @@ for location in file_locations:
     location = location + ".0" if os.path.exists(location) else location + ".1"
     new_file_locations.append(location)
 
+# Now, let's check which directories don't exist
+for location in new_file_locations:
     if not os.path.exists(location):
         non_existent_directories.append(location)
 
-# Write the modified list of file locations back to the input file
-with open(input_file, "w") as file:
-    file.writelines("\n".join(new_file_locations)
-
-# Save the non-existent directories to not_found.txt
+# Write the non-existent directories to not_found.txt
 if non_existent_directories:
     with open(not_found_file, "w") as not_found:
         not_found.writelines("\n".join(non_existent_directories))
 
-# Now, remove the non-existent directories from the original list
-file_locations = [location for location in file_locations if location not in non_existent_directories]
+# Remove non-existent directories from dbw.dat
+new_file_locations = [location for location in new_file_locations if os.path.exists(location)]
 
-# Write the cleaned list of file locations back to the input file
+# Write the modified list of file locations back to the input file
 with open(input_file, "w") as file:
-    file.writelines("\n".join(file_locations))
+    file.writelines("\n".join(new_file_locations))
 
 print("Directories that don't exist have been saved in not_found.txt and removed from", input_file)
