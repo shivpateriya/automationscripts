b33c06b940817839daa5a047c63dbaab78096e5b
shivpateriya <shivampateriya40@gmail.com>
2023-10-27 13:37:00 +0530

    add file

--- final_dbw.py
+++ final_dbw.py
@@ -1,8 +1,9 @@
 import os
+import glob
+import re
 
 # Define the name of the input file
 input_file = "dbw.dat"
-not_found_file = "not_found.txt"
 
 # Read the list of file locations from the input file
 with open(input_file, "r") as file:
@@ -12,27 +13,29 @@ with open(input_file, "r") as file:
 for i in range(len(file_locations)):
     file_locations[i] = file_locations[i].rsplit('.', 1)[0]
 
-# Now, let's check which directories don't exist
+# Now, let's check which directories don't exist and handle wildcards
 new_file_locations = []
 
 for location in file_locations:
-    location = location + ".0" if os.path.exists(location) else location + ".1"
+    # Replace "hdb", "hdb2", "hdb3", or "hdb4" with "hdb*"
+    location_with_wildcard = re.sub(r"hdb[2-4]?", "hdb*", location)
+
+    # Use glob to check if any files or directories match the wildcard location with ".*" added
+    matching_files = glob.glob(location_with_wildcard + ".*")
+
+    if matching_files:
+        # If matching files exist, use the first matching file's absolute path
+        location = os.path.abspath(matching_files[0])
+    else:
+        location = os.path.abspath(location)  # Use the original location if no matches found
+
     new_file_locations.append(location)
 
-# Write the non-existent directories to not_found.txt
-non_existent_directories = [location for location in new_file_locations if not os.path.exists(location)]
-if non_existent_directories:
-    with open(not_found_file, "w") as not_found:
-        not_found.writelines("\n".join(non_existent_directories))
-
-# Remove non-existent directories from dbw.dat
-new_file_locations = [location for location in new_file_locations if os.path.exists(location)]
-
-# Add "partition" at the beginning
+# Insert "partition" as the first line
 new_file_locations.insert(0, "partition")
 
-# Write the modified list of file locations back to the input file
+# Write the modified absolute paths back to the input file
 with open(input_file, "w") as file:
     file.writelines("\n".join(new_file_locations))
 
-print("File locations have been modified in-place and non-existent directories removed from", input_file)
+print("File locations have been updated in", input_file)
