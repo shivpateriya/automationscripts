f4ace12616044a1c6ca91c0aeaa49daad6b6ff77
shivpateriya <shivampateriya40@gmail.com>
2023-11-03 16:20:52 +0530

    new message

--- logfile.py
+++ logfile.py
@@ -1,9 +1,8 @@
-import subprocess
 import os
 from datetime import datetime
 
-# Change the current working directory to "cdl"
-os.chdir("cdl")
+# Run the "cdl" command if it's a valid executable
+os.system("cdl")  # Replace this with the correct command and its arguments
 
 # Get the current year and month
 current_date = datetime.now()
@@ -16,4 +15,15 @@ user_date = f"{year}{month:02}"
 # Construct the grep command
 pattern = f"*DBW*{user_date}*"
 grep_command = f'grep EOD {pattern} | grep elapsed | grep date'
-subprocess.run(["bash", "-c", grep_command], shell=True)
+
+# Run the grep command using os.popen() to capture its output
+output = os.popen(grep_command).read()
+
+# Check if there's any output and print accordingly
+if output:
+    print("Logs found for this date:")
+    # Extract the part of the output until ".log"
+    logs = [line.split('.log')[0] + '.log' for line in output.split('\n')]
+    print("\n".join(logs))
+else:
+    print("No logs found for this date.")
