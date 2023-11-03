f5dd52a6b8602b3521f12bcb4922f43a745261c4
shivpateriya <shivampateriya40@gmail.com>
2023-11-03 16:54:40 +0530

    add file

--- logfile.py
+++ logfile.py
@@ -1,29 +1,40 @@
 import os
+import glob
+import re
+import subprocess
 from datetime import datetime
 
-# Run the "cdl" command if it's a valid executable
-os.system("cdl")  # Replace this with the correct command and its arguments
+def process_logs_and_update_dbw_dat(user_input):
+    os.chdir("/apps/kx/delta-data/DeltaControlData/logdir")
 
-# Get the current year and month
-current_date = datetime.now()
-year = current_date.year
-month = current_date.month
+    current_date = datetime.now()
+    year = current_date.year
+    month = current_date.month
 
-# Construct the date in the format "YYYYMM" (e.g., "202311" for November 2023)
-user_date = f"{year}{month:02}"
+    Eod_month = f"{year}{month:02}"
 
-# Construct the grep command
-pattern = f"*DBW*{user_date}*"
-grep_command = f'grep EOD {pattern} | grep elapsed | grep date'
+    pattern = f"*DBW*{Eod_month}*"
+    print(pattern)
 
-# Run the grep command using os.popen() to capture its output
-output = os.popen(grep_command).read()
+    grep_command = f'grep EOD {pattern} | grep elapsed | grep {user_input}'
 
-# Check if there's any output and print accordingly
-if output:
-    print("Logs found for this date:")
-    # Extract the part of the output until ".log"
-    logs = [line.split('.log')[0] + '.log' for line in output.split('\n')]
-    print("\n".join(logs))
-else:
-    print("No logs found for this date.")
+    output = os.popen(grep_command).read()
+    exit_code = os.system(grep_command)
+
+    if output:
+        print("Logs found for this date:")
+        logs = [line.split('.log')[0] + '.log' for line in output.split('\n')]
+        logs = logs[0]
+
+        create_dbw_dat = f"grep 'DBW Write :' {logs} | grep kxsReading | grep complete | awk '{{print $7}}' | sed 's/://' | sed 's/\/$//' | grep -v apps > ~/Guardians/shivam/dbw.dat"
+        os.system(create_dbw_dat)
+    else:
+        print("No logs found for this date.")
+
+# Call the function with user input
+user_input = input("Enter the date (format: 2023.11.02): ")
+process_logs_and_update_dbw_dat(user_input)
+
+# Rest of your original code
+input_file = "dbw.dat"
+# ... (the rest of your code remains unchanged)
